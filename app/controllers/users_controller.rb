7d94e0783c02eeda73e049b5b4a9ac7c94c4a6b4
miyaryj <miya.ryj@gmail.com>
2014-09-10 21:49:50 +0900

    Practice 9.6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
+  before_action :guest_user,     only: [:new, :create]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -65,6 +66,10 @@ class UsersController < ApplicationController
 
     def admin_user
       redirect_to(root_path) unless current_user.admin?
+    end
+
+    def guest_user
+      redirect_to(root_path) if signed_in?
     end
 
 end
